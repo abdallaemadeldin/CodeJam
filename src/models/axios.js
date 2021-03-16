3962227616d519d7067c0b2b4829f908da2ada7a
Abdalla Emad <abdalla.emad.eldin@gmail.com>
2021-03-17 00:23:06 +0200

    add axios modeler

--- src/models/axios.js
+++ src/models/axios.js
@@ -16,38 +16,3 @@ export const get = (url, options, token) => {
             }
         });
 }
-
-export const post = (url, params, options, token) => {
-    let requestURL = `${baseURL}${url}`;
-    let CancelToken = axios.CancelToken;
-
-    axios.post(requestURL, params, {
-        headers: token ? { Authorization: `Bearer ${token}` } : {},
-        onDownloadProgress: (progress) => {
-            if (options.onDownloadProgress) {
-                options.onDownloadProgress(progress);
-            }
-        },
-        onUploadProgress: (progress) => {
-            if (options.onUploadProgress) {
-                options.onUploadProgress(progress);
-            }
-        },
-        cancelToken: new CancelToken((cancel) => {
-            if (options.cancel) {
-                options.cancel(cancel);
-            }
-        }),
-        timeout: parseInt(timeout)
-    })
-        .then(response => {
-            if (options.success) {
-                options.success(response.data);
-            }
-        })
-        .catch(error => {
-            if (options.error) {
-                options.error(error);
-            }
-        });
-}
